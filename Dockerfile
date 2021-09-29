b9d57413780d26ce55d2a77ee31bb631cc06678e
Aniket Palve <light@Anikets-MacBook-Air.local>
2021-09-30 00:54:48 +0530

    Added API endpoints to create user and Tokens

--- Dockerfile
+++ Dockerfile
@@ -8,9 +8,9 @@ COPY ./requirements.txt /requirements.txt
 
 # Install dependencies
 COPY ./requirements.txt /requirements.txt
-RUN apk add --update --no-cache postgresql-client
+RUN apk add --update --no-cache postgresql-client jpeg-dev
 RUN apk add --update --no-cache --virtual .tmp-build-deps \
-      gcc libc-dev linux-headers postgresql-dev
+      gcc libc-dev linux-headers postgresql-dev musl-dev zlib zlib-dev
 RUN pip install -r /requirements.txt
 RUN apk del .tmp-build-deps
 
--- app/core/models.py
+++ app/core/models.py
@@ -3,30 +3,28 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password=None, **kwargs):
-        if email is None:
-            raise ValueError
-        user = self.model(email= self.normalize_email(email), **kwargs)
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
-
 
     def create_superuser(self, email, password):
-
-        user = self.model(email=email, password=password)
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
-        
 
 class User(AbstractBaseUser, PermissionsMixin):
-
+    """Custom user model that suppors using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
